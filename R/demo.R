4d4ff2a44a9e116e39f1b33f2b205869860bf0ca
Mauro Lepore <maurolepore@gmail.com>
2020-05-05 20:34:22 +0000

    Warm up challenges

--- R/demo.R
+++ R/demo.R
@@ -1,1 +1,20 @@
-# Live code starts here
+# Warm up -----------------------------------------------------------------
+
+# 1. What **arithmetic operators** do you expect R to have? Test your predictions
+1
+1 + 1
+
+# 2. What does "<-" do?
+# 3. What **assignment operators** do you expect R to have? Test your predictions
+x <- 3 * 4
+x
+
+# 4. What does ">" do?
+# 5. What **logical operators** do you expect R to have? Test your predictions
+3 * 4 > x
+x
+
+# 6. What is the difference between x and y?
+# 7. What basic **data types** do you expect R to have?
+x <- 1
+y <- "one"
